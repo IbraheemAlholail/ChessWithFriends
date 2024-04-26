caf921d238fe9b455be29d4641ee3f948b17e4e0
Alex P <warmishfrog@gmail.com>
2024-04-26 21:29:10 +0100

    Drag and snap working
    with anti piece layering working :)

--- Assets/Scripts/DragAll.cs
+++ Assets/Scripts/DragAll.cs
@@ -1,3 +1,4 @@
+using System.CodeDom;
 using System.Collections;
 using System.Collections.Generic;
 using System.Diagnostics;
@@ -7,9 +8,12 @@ public class DragAll : MonoBehaviour
 { 
     private Transform dragging = null;
     private Vector3 offset;
+
     [SerializeField] private LayerMask draggableLayers;
     [SerializeField] private LayerMask boardTileLayers;
     [SerializeField] private float gridSize = 0.25f;
+
+    GameObject tile;
 
     void Update()
     {
@@ -28,22 +32,17 @@ public class DragAll : MonoBehaviour
         }
         else if (Input.GetMouseButtonUp(0)) //stop dragging
         {
-            dragging = null;
-        }
-
-        if (dragging != null) //move object
-        {
             // Snap to the nearest grid position
             Vector3 snappedPosition = SnapToGrid(Camera.main.ScreenToWorldPoint(Input.mousePosition) + offset);
 
             // Get the tile under the snapped position
-            GameObject tile = GetTileAtPosition(snappedPosition);
+            tile = GetTileAtPosition(snappedPosition);
 
             if (tile != null)
             {
                 // Make the dragged piece a child of the tile
                 dragging.SetParent(tile.transform);
-                dragging.localPosition = new Vector3(0,0,1); // Center the piece on the tile
+                dragging.localPosition = new Vector3(0, 0, 1); // Center the piece on the tile
 
                 //UnityEngine.Debug.Log("Piece dropped on tile: " + tile.name);
             }
@@ -51,6 +50,12 @@ public class DragAll : MonoBehaviour
             {
                 UnityEngine.Debug.LogWarning("No tile found under dropped position.");
             }
+            dragging = null;            
+        }
+
+        if (dragging != null) //move object
+        {
+            dragging.position = Camera.main.ScreenToWorldPoint(Input.mousePosition) + offset;
         }
     }
 
@@ -68,7 +73,14 @@ public class DragAll : MonoBehaviour
         RaycastHit2D hit = Physics2D.Raycast(position, Vector2.zero, 0.1f, boardTileLayers);
         if (hit)
         {
-            return hit.transform.gameObject;
+            if (hit.transform.childCount == 0)
+            {
+                return hit.transform.gameObject;
+            }
+            else
+            {
+                UnityEngine.Debug.LogWarning("Tile already has a piece on it.");
+            }
         }
         return null;
     }
